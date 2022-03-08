546991adeafeee3e6c2ff964e6c9690058c0e4a9
lazyensan <97110269+lazyensan@users.noreply.github.com>
2022-03-08 11:43:19 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.retrofitapicall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.invoke.MethodHandle;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getData;
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getData = findViewById(R.id.get_data);
+        getData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Methods methods = RetrofitClient.getRetrofitInstance().create(Methods.class);
+                Call<Model> call = methods.getAllData();
+
+                call.enqueue(new Callback<Model>() {
+                    @Override
+                    public void onResponse(Call<Model> call, Response<Model> response) {
+                        Log.e(TAG, "onResponse: code: " +response.code());
+
+
+                        ArrayList<Model.data> data = response.body().getData();
+
+                        for(Model.data data1 : data){
+                            Log.e(TAG, "onResponse: emails : " +data1.getEmail());
+
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Model> call, Throwable t) {
+                        Log.e(TAG, "onFailure: code: " +t.getMessage());
+
+                    }
+                });
+            }
+        });
+
+
+    }
+}
